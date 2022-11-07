4eb0d4ca59eb8b1c6bd1b7cb77ac5f03ff53e58d
loquitov <davidcalvo00@iesamachado.org>
2022-11-07 13:51:16 +0100

    me cago en dios

--- dni.js
+++
@@ -1,6 +0,0 @@
-function letra() {
-  cadena = "TRWAGMYFPDXBNJZSQVHLCKET";
-  posicion = formulario.dni.value % 23;
-  letra = cadena.substring(posicion, posicion + 1);
-  document.formulario.dni.value = formulario.dni.value + " - " + letra;
-}
